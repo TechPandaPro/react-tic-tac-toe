61cb6c39fb678f281679e53966939b679c929672
TechPandaPro <javaisntcoffee@gmail.com>
2024-07-15 10:16:47 -0700

    Use loops for Board instead of hardcoding

--- src/App.js
+++ src/App.js
@@ -22,24 +22,29 @@ function Board({ xIsNext, squares, onPlay }) {
   if (winner) status = `Winner: ${winner}`;
   else status = `Next player: ${xIsNext ? "X" : "O"}`;
 
+  const boardRows = Array(3)
+    .fill()
+    .map((_, i) => (
+      <div key={i} className="board-name">
+        {Array(3)
+          .fill()
+          .map((_, j) => {
+            const squareNum = 3 * i + j;
+            return (
+              <Square
+                key={squareNum}
+                value={squares[squareNum]}
+                onSquareClick={() => handleClick(squareNum)}
+              />
+            );
+          })}
+      </div>
+    ));
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {boardRows}
     </>
   );
 }
