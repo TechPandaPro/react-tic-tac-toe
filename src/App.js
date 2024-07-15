2b875e00a1ba5eda2a4c0e2098fde497fb57ee44
TechPandaPro <javaisntcoffee@gmail.com>
2024-07-15 10:31:37 -0700

    Add sort order toggle

--- src/App.js
+++ src/App.js
@@ -52,6 +52,7 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const [ascending, setAscending] = useState(true);
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
@@ -63,6 +64,10 @@ export default function Game() {
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
+  }
+
+  function handleToggleClick() {
+    setAscending(!ascending);
   }
 
   const moves = history.map((squares, move) => {
@@ -78,6 +83,8 @@ export default function Game() {
       );
   });
 
+  if (!ascending) moves.reverse();
+
   return (
     <div className="game">
       <div className="game-board">
@@ -85,6 +92,7 @@ export default function Game() {
       </div>
       <div className="game-info">
         <ol>{moves}</ol>
+        <button onClick={handleToggleClick}>Toggle Sorting Order</button>
       </div>
     </div>
   );
